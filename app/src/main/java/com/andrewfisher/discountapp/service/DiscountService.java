3b9ad79b96dc9adb489aa42c005a3dd1b359639c
Andrew Fisher <AndrewFishersb@gmail.com>
2017-01-12 10:49:57 -0800

    read json file into the service

--- app/src/main/java/com/andrewfisher/discountapp/service/DiscountService.java
+++ app/src/main/java/com/andrewfisher/discountapp/service/DiscountService.java
@@ -1,8 +1,56 @@
 package com.andrewfisher.discountapp.service;
 
+import android.content.Context;
+import android.util.Log;
+
+import com.andrewfisher.discountapp.model.DiscountItem;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
 /**
  * Created by andrewfisher on 1/12/17.
  */
 
 public class DiscountService {
+    private Context mContext;
+
+    //Retrieves the Json file from the assets folder, and sets it to a string
+    public String getJSONFile(){
+        String jsonFileString;
+        try{
+            InputStream asset = mContext.getAssets().open("discounts.json");
+            int size = asset.available();
+            byte[] buffer = new byte[size];
+            asset.read(buffer);
+            asset.close();
+            jsonFileString = new String(buffer, "UTF-8");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        Log.d("test",jsonFileString);
+        return jsonFileString;
+    }
+
+
+    public ArrayList<DiscountItem> allDiscounts(){
+        String jsonString = getJSONFile();
+
+
+
+
+
+
+        try{
+            JSONObject json = new JSONObject(jsonString);
+        }
+    }
+
+
+
 }
